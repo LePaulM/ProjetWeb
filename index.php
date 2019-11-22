8a57d3ed2912849cc0ddad81e6e3577989ecd090
LePaulM <p.miancien@laposte.net>
2019-11-22 20:28:12 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="UTF-8">
+    <title>Mon jeu géographique</title>
+    <link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet/master/leaflet.css" />
+    <script src="http://cdn.leafletjs.com/leaflet/master/leaflet.js"></script>
+   <link rel= "stylesheet" href="jeuGeo.css"/>
+  </head>
+  <body>
+    <p>Le texte de mon premier paragraphe</p>
+    <p id=trouver>Nombre d'éngmes résolues : 0/21</p>
+    <input type="submit" name="jouer" value="Jouer">
+    <div id=map> </div>
+    <script src=jeuGeo.js></script>
+  </body>
+</html>
--- jeuGeo.js
+++ jeuGeo.js
@@ -1,9 +1,52 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+// Déclarer la carte
+var map = L.map('map').setView([48.83889, 2.588889],13);
 
+// Ajouter la couche "Carte"
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    maxZoom: 19,
 }).addTo(map);
 
+// permettra d'afficher les énigmes
+var popup = L.popup();
+
+// permettra d'accéder au HTML du nombre d'objets trouvés
+var texte = document.getElementById('trouver');
+
+// permettra de mettre à jour le nombre d'objets trouvés
+var nbClick = 0;
+
+function onMapClick(e) {
+  // permet de mettre une popup à l'endroit d'un clic.
+  // S'en servir pour afficher l'énogme suivante lorsque le joueur résoud une énigme
+  // ajoute également un au nombre d'énigmes résolues
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
+    nbClick++;
+    texte.innerHTML = "Nombre d'éngmes résolues : " + nbClick + "/21"
+
+}
+//map.on('click', onMapClick);    permet d'appliquer la fonction ci-dessus
+
+// permet de mettre une icone à la place de l'icone standard des marker
+var greenIcon = L.icon({
+    iconUrl: 'earth-1706130_1920.png',
+    //shadowUrl: 'leaf-shadow.png',
+
+    iconSize:     [95, 95], // size of the icon
+    //shadowSize:   [50, 64], // size of the shadow
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    //shadowAnchor: [4, 62],  // the same for the shadow
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+//permet d'afficher un marker avec pour icone une image (celle de l'URL)
+L.marker([48.805,2.231111], {icon: greenIcon}).addTo(map);
+
+
+/*
 L.marker([48.83889, 2.588889]).addTo(map)
     .bindPopup('"Ler premier indice".')
     .openPopup();
+*/
