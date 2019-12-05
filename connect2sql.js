37aebba75bb5b3274c040f4ce8d314acb9cacfbe
ECorvi0 <48996058+ECorvi0@users.noreply.github.com>
2019-12-05 14:21:41 +0100

    Create connect2sql.js

---
+++ connect2sql.js
@@ -0,0 +1,26 @@
+// config for your database
+var config = {
+    user: 'database',
+    password: 'mypassword',
+    server: 'localhost', 
+    database: 'bdd.txt'
+};
+
+// connect to your database
+sql.connect(config, function (err) {
+
+    if (err) console.log(err);
+
+    // create Request object
+    var request = new sql.Request();
+
+    // query to the database and get the records
+    request.query('select * from Student', function (err, recordset) {
+
+        if (err) console.log(err)
+
+        // send records as a response
+        res.send(recordset);
+
+    });
+});
